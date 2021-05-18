adf02e88ee59ce3ce7337a43d7217235e041c52f
AnuragSahu <anuragatiit@gmail.com>
2021-05-18 23:50:40 +0530

    Fully Working need to Change

---
+++ client/js/script.js
@@ -0,0 +1,270 @@
+$(document).ready(function () {
+    "use strict";
+
+    function getUsers() {
+        $.ajax({
+            url: "/get-users",
+            dataType: "json",
+            type: "GET",
+            success: function (data) {
+                console.log(data);
+                let newRow = `<table class= "table table-hover table-dark table-striped" id="users"><tr>
+                <th class="id_header"><span>ID</span></th>
+                <th class="name_header"><span>Name</span></th>
+                <th class="location_header"><span>Location</span></th>
+                <th class="gas_header"><span>Gas Vehicle (Yes / No)</span></th>
+                <th class="emissions_header"><span>Emission Count (in CO2 tons)</span></th>
+                <th class="delete_header"><span>Click to Delete</span></th>
+                </tr>`;
+
+                for (let i = 0; i < data.rows.length; i++) {
+                    let row = data.rows[i];
+                    newRow += ("<tr><td class='userID'>" + row.userID +
+                        "</td><td class='userName'><span>" + row.userName +
+                        "</span></td><td class='location'><span>" + row.location +
+                        "</span></td><td class='gasBool'><span>" + row.gasBool +
+                        "</span></td><td class='emission'><span>" + row.emission +
+                        "</span></td><td class='delete'><button id = '" + row.userID + "' class = 'btn btn-danger delBtn'>╳</button> </td></tr>");
+                }
+                newRow += "</table>";
+                $("#container").html(newRow);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    }
+    getUsers();
+
+    $('#submit-btn').click(function (e) {
+        e.preventDefault();
+
+        let formData = {
+            userID: $("#userID").val(),
+            userName: $("#userName").val(),
+            location: $("#location").val(),
+            gasBool: $("#gasBool").val(),
+            emission: $("#emission").val()
+        };
+        console.log($("#userID").val(""));
+        $("#userID").val("");
+        $("#userName").val("");
+        $("#location").val("");
+        $("#gasBool").val("");
+        $("#emission").val("");
+
+        $.ajax({
+            url: "/add-user",
+            dataType: "json",
+            type: "POST",
+            data: formData,
+            success: function (data) {
+                $("#requestStatus").html("The user's data was added to the database.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#container').on('click', 'span', function () {
+
+        if ($(this).parent().attr('class') === 'gasBool') {
+            let spanText = $(this).text();
+            let td = $(this).parent();
+            let input = $("<input type='text' value='" + spanText + "'>");
+
+            td.html(input);
+            $(input).keyup(function (e) {
+                let val = null;
+                let span = null;
+                if (e.which == 13) {
+                    val = $(input).val();
+                    span = $("<span>" + val + "</span>");
+                    td.html(span);
+
+                    console.log(td.parent().find("[class='userID']")[0]);
+
+                    let dataToServer = {
+                        id: td.parent().find("[class='userID']").html(),
+                        gasBool: val
+                    };
+
+                    $.ajax({
+                        url: "update-userBool",
+                        dataType: "json",
+                        type: "POST",
+                        data: dataToServer,
+                        success: function (data) {
+                            $("#requestStatus").html("The data was updated on the database.");
+                            getUsers();
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            $("#p2").text(jqXHR.statusText);
+                            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    $('#container').on('click', 'span', function () {
+
+        if ($(this).parent().attr('class') === 'userName') {
+            let spanText = $(this).text();
+            let td = $(this).parent();
+            let input = $("<input type='text' value='" + spanText + "'>");
+
+            td.html(input);
+            $(input).keyup(function (e) {
+                let val = null;
+                let span = null;
+                if (e.which == 13) {
+                    val = $(input).val();
+                    span = $("<span>" + val + "</span>");
+                    td.html(span);
+
+                    console.log(td.parent().find("[class='userID']")[0]);
+
+                    let dataToServer = {
+                        id: td.parent().find("[class='userID']").html(),
+                        userName: val
+                    };
+
+                    $.ajax({
+                        url: "update-userName",
+                        dataType: "json",
+                        type: "POST",
+                        data: dataToServer,
+                        success: function (data) {
+                            $("#requestStatus").html("The data was updated on the database.");
+                            getUsers();
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            $("#p2").text(jqXHR.statusText);
+                            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    $('#container').on('click', 'span', function () {
+
+        if ($(this).parent().attr('class') === 'location') {
+            let spanText = $(this).text();
+            let td = $(this).parent();
+            let input = $("<input type='text' value='" + spanText + "'>");
+
+            td.html(input);
+            $(input).keyup(function (e) {
+                let val = null;
+                let span = null;
+                if (e.which == 13) {
+                    val = $(input).val();
+                    span = $("<span>" + val + "</span>");
+                    td.html(span);
+
+                    console.log(td.parent().find("[class='userID']")[0]);
+
+                    let dataToServer = {
+                        id: td.parent().find("[class='userID']").html(),
+                        location: val
+                    };
+
+                    $.ajax({
+                        url: "update-userLocation",
+                        dataType: "json",
+                        type: "POST",
+                        data: dataToServer,
+                        success: function (data) {
+                            $("#requestStatus").html("The data was updated on the database.");
+                            getUsers();
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            $("#p2").text(jqXHR.statusText);
+                            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    $(document).on('click', '.delBtn', function(){
+
+        let deleteButton = $(this);
+
+        console.log(deleteButton.attr('id'));
+
+        let dataToServer = {
+            userID: deleteButton.attr('id')
+        };
+        // id: td.parent().find("[class='userID']").html()
+        $.ajax({
+            url: "/delete-user",
+            dataType: "json",
+            type: "POST",
+            data: dataToServer,
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record Deleted");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+
+        });
+    });
+
+    $('#container').on('click', 'span', function () {
+
+        if ($(this).parent().attr('class') === 'emission') {
+            let spanText = $(this).text();
+            let td = $(this).parent();
+            let input = $("<input type='text' value='" + spanText + "'>");
+
+            td.html(input);
+            $(input).keyup(function (e) {
+                let val = null;
+                let span = null;
+                if (e.which == 13) {
+                    val = $(input).val();
+                    span = $("<span>" + val + "</span>");
+                    td.html(span);
+
+                    console.log(td.parent().find("[class='userID']")[0]);
+
+                    let dataToServer = {
+                        id: td.parent().find("[class='userID']").html(),
+                        emission: val
+                    };
+
+                    $.ajax({
+                        url: "update-userEmission",
+                        dataType: "json",
+                        type: "POST",
+                        data: dataToServer,
+                        success: function (data) {
+                            $("#requestStatus").html("The data was updated on the database.");
+                            getUsers();
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            $("#p2").text(jqXHR.statusText);
+                            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+
+});
---
+++ server.js
@@ -0,0 +1,272 @@
+"use strict";
+// REQUIRES
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+
+const msg404 = 'Not Found';
+
+app.use('/js', express.static('client/js'))
+app.use('/css', express.static('client/css'))
+app.use('/img', express.static('client/img'))
+
+
+
+
+app.get('/', function (req, res) {
+
+
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    multipleStatements: true
+  });
+
+  const createDBAndTables = `CREATE DATABASE IF NOT EXISTS assignment;
+  use assignment;
+  CREATE TABLE IF NOT EXISTS appUser (
+  userID varchar(15) NOT NULL,
+  userName varchar(20),
+  location varchar(30),
+  gasBool varchar(30),
+  emission int(10),
+  PRIMARY KEY (userID));
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('mhav1', 'madhav', 'vancouver', 'no', '23');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('slando', 'sam', 'burnaby', 'yes', '27');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('jryue', 'john', 'surrey', 'no', '12');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('hsaini123', 'harleen', 'vancouver', 'no', '25');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('aFerg101', 'aaron', 'richmond', 'yes', '32');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('canucksfan7', 'dustin', 'north vancouer', 'yes', '23');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('ecoHenry', 'henry', 'vanouver', 'yes', '14');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('scottVan1996', 'scott', 'downtown', 'no', '87');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('smithy12', 'smith', 'burnaby', 'no', '27');
+  INSERT IGNORE INTO appUser (userID, userName, location, gasBool, emission) values ('thisJustIn', 'justin', 'surrey', 'yes', '23');`;
+
+  connection.connect();
+  connection.query(createDBAndTables, function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+
+  });
+  connection.end();
+
+  let doc = fs.readFileSync('./client/html/index.html', "utf8");
+  res.send(doc);
+});
+
+
+app.get('/get-users', function (req, res) {
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+  connection.query('SELECT * FROM appUser', function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('Rows returned are: ', results);
+    res.send({
+      status: "success",
+      rows: results
+    });
+
+  });
+  connection.end();
+
+
+});
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+
+app.post('/add-user', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  console.log("username", req.body.userName);
+  console.log("location", req.body.location);
+  console.log("gas vehicle", req.body.gasBool);
+  console.log("emissions", req.body.emission);
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('INSERT INTO appUser (userID, userName, location, gasBool, emission) values (?, ?, ?, ?, ?)',
+    [req.body.userID, req.body.userName, req.body.location, req.body.gasBool, req.body.emission],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded added."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/update-userBool', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('UPDATE appUser SET gasBool = ? WHERE userID = ?',
+    [req.body.gasBool, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded updated."
+      });
+
+    });
+  connection.end();
+
+});
+app.post('/update-userName', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('UPDATE appUser SET userName = ? WHERE userID = ?',
+    [req.body.userName, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded updated."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/update-userLocation', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('UPDATE appUser SET location = ? WHERE userID = ?',
+    [req.body.location, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded updated."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/update-userEmission', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('UPDATE appUser SET emission = ? WHERE userID = ?',
+    [req.body.emission, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded updated."
+      });
+
+    });
+  connection.end();
+
+});
+
+
+app.post('/delete-user', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password@123',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('DELETE FROM appUser WHERE userID = ?',
+    [req.body.userID],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+       console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded deleted."
+      });
+
+    });
+  connection.end();
+
+});
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, function () {
+  console.log('App listening on port ' + port + '!');
+})
